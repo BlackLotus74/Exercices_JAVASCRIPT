d38688c3cf62629a598cc41b7eb57b596365e5c4
François <francoislancian@gmail.com>
2019-08-29 17:08:53 +0200

    commit 29/08/19 rgb work

--- CRM_Exo_JS09/changeColor.js
+++ CRM_Exo_JS09/changeColor.js
@@ -6,5 +6,12 @@ function displayName(color){
     document.getElementById("nomRouge").innerHTML["nomRouge"].value=color;
 }
 
+function rgb(){
+    document.getElementById("content").style.backgroundColor =
+    "rgb("+document.getElementById("nomRouge").value+","
+    +document.getElementById("nomVert").value+","
+    +document.getElementById("nomBleu").value+")";
+}
+
 
 
