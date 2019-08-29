1e424e6ded2656142c761cdc42e697e86938b8f0
François <francoislancian@gmail.com>
2019-08-29 16:53:32 +0200

    cour crm js FC 29/08/19 part 02

--- CRM_Exo_JS09/changeColor.js
+++ CRM_Exo_JS09/changeColor.js
@@ -2,4 +2,9 @@ function changeColor(color){
     document.getElementById("content").style.background = color;
 }
 
+function displayName(color){
+    document.getElementById("nomRouge").innerHTML["nomRouge"].value=color;
+}
+
+
 
