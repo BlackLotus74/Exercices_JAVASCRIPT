23c1017aa276fb5acd971e29e8e7670f23e04308
François <francoislancian@gmail.com>
2019-08-30 12:05:57 +0200

    exo_crm_changeColor_correction

--- CRM_Exo_JS09/changeColor.js
+++ CRM_Exo_JS09/changeColor.js
@@ -2,10 +2,6 @@ function changeColor(color){
     document.getElementById("content").style.background = color;
 }
 
-function displayName(color){
-    document.getElementById("nomRouge").innerHTML["nomRouge"].value=color;
-}
-
 function rgb(){
     document.getElementById("content").style.backgroundColor =
     "rgb("+document.getElementById("nomRouge").value+","
@@ -13,5 +9,31 @@ function rgb(){
     +document.getElementById("nomBleu").value+")";
 }
 
+//correction
+function testChamps (_champs){
+    var chaine = document.getElementById(_champs).value.toUpperCase();
+    var regex = /^[0-9A-F]{2}$/;
+    var testBool = false;
 
+    if(regex.test(chaine)){
+        testBool = true;
+    }else{
+        document.getElementById(_champs).value="";
+        document.getElementById(_champs).focus();
+        alert("2 caractères en code hexa");
+    }
+    return testBool;
+}
+
+function genereCouleur(_champs){
+    if(testChamps(_champs) == true){
+        var couleur = "#" + document.querySelector("#rouge").value
+        + document.querySelector("#vert").value
+        + document.querySelector("#bleu").value;
+
+        document.body.style.backgroundColor = couleur;
+    }
+
+document.querySelector("#rouge").addEventListener("blur", function(){genereCouleur("rouge");});
+}
 
