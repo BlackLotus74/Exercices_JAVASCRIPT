8ae5dd0063493c26437768a88eb95c19ec644773
François <francoislancian@gmail.com>
2020-01-03 10:37:13 +0100

    commit vue.js et autres 03/01/2020

---
+++ TUTO_Vue.js_grafikart/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'salut les gens !',
+        link: 'http://grafikart.fr',
+        //success: true,
+        cls: 'success',
+        persons: ['john', 'françois', 'marion', 'caroline', 'jean', 'lucy']
+    },
+
+    methods: {
+        close: function(){
+            this.success = false
+        }
+    }
+})
