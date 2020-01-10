505fb159626a7caf3fbaedf5ed52cf6ebf37eb23
François <francoislancian@gmail.com>
2020-01-10 11:57:32 +0100

    commit 10/01/2020 vue v02

--- TUTO_Vue.js_grafikart/app3.js
+++ TUTO_Vue.js_grafikart/app3.js
@@ -1,11 +1,12 @@
 let vm = new Vue({
     el: '#app3',
     data: {
-        success: false
+        success: false,
+        message: ''
     },
-    methods: {
+    computed: {  //gros avantage sur performances /!\ - met en "cache" les valeurs (methode computed) par defaut que des geter - on ne peut pas redefinir variable cls
         cls: function(){
-            
+
             console.log('cls called')
 
             return this.success === true ? 'success' : 'error'
