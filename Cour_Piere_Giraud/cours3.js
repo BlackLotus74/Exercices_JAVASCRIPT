5c95b79b1d343e4417af2c658139810fb97af4c6
Black_Lotus74 <francoislancian@gmail.com>
2020-01-11 19:38:29 +0100

    commit js pg 03

---
+++ Cour_Piere_Giraud/cours3.js
@@ -0,0 +1,37 @@
+/*let nul = null;
+let ind;
+
+document.getElementById('p1').innerHTML = 'type de nul :' + typeof nul;
+document.getElementById('p2').innerHTML = 'type de ind :' + typeof ind;
+
+let x = 2;
+let y = 3;
+let z = 4;
+
+let a = x +1;
+let b = x + y;
+let c = x - y;
+let d = x * y;
+let e = x / y;
+let f = 5 % 3;
+let g = x ** 3;
+
+alert('a contient : ' + a +
+    '\nb contient : ' + b +
+    '\nc contient : ' + c +
+    '\nd contient : ' + d +
+    '\ne contient : ' + e +
+    '\nf contient : ' + f +
+    '\ng contient : ' + g
+);*/
+
+let x = 5;
+let y = 50;
+
+alert('x contient ' + x +
+    '\ny contient ' + y +
+    '\nleur somme vaut ' + (x + y));
+
+alert(`x contient ${x}
+    y contient ${y}
+    leur somme vaut ${x + y}`);
