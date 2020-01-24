74da9e6e766c3f7e09c73b2bb1c185de8d477932
François <francoislancian@gmail.com>
2020-01-24 11:37:13 +0100

    commit 24/01/2020 suite v02

--- TUTO_Vue.js_grafikart/app3.js
+++ TUTO_Vue.js_grafikart/app3.js
@@ -1,15 +1,32 @@
 let vm = new Vue({
     el: '#app3',
     data: {
-        success: false,
-        message: ''
+        /*success: false,
+        message: '',*/
+        firstname: 'Jean',
+        lastname: 'DelaTour'
     },
-    computed: {  //gros avantage sur performances /!\ - met en "cache" les valeurs (methode computed) par defaut que des geter - on ne peut pas redefinir variable cls
-        cls: function(){
 
-            console.log('cls called')
+    computed: { //gros avantage sur performances /!\ - met en "cache" les valeurs (methode computed) par defaut que des geter - on ne peut pas redefinir variable cls
+        fullName: {
+            get: function () {
+                return this.firstname + ' ' + this.lastname
+            },
 
-            return this.success === true ? 'success' : 'error'
+            set: function (value) {
+                console.log(value)
+                let parts = value.split(' ')
+                this.firstname = parts[0]
+                this.lastname = parts[1]
+            }
         }
     }
+
+    /*cls: function(){
+
+        console.log('cls called')
+
+        return this.success === true ? 'success' : 'error'
+    }*/
+
 })
