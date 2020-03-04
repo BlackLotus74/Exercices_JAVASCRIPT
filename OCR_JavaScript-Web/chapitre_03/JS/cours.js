7a4373da457e432a85068e19b4334652530e155e
François <francoislancian@gmail.com>
2020-03-04 16:48:51 +0100

    commit 04/03/2020 suite cours js ocr

--- OCR_JavaScript-Web/chapitre_03/JS/cours.js
+++ OCR_JavaScript-Web/chapitre_03/JS/cours.js
@@ -15,9 +15,9 @@ document.querySelector("h1").id = "titre";
 
 
 //ajouter ou supprimer des classes
-let titreElt = document.querySelector('h1'); //acces au premier titre h1
-titreElt.classList.remove("debut");          //suppression de la classe "debut"
-titreElt.classList.add("titre");             //ajout de la classe "titre"
+let titreElt = document.querySelector('h1');              //acces au premier titre h1
+titreElt.classList.remove("debut");                       //suppression de la classe "debut"
+titreElt.classList.add("titre");                          //ajout de la classe "titre"
 console.log(titreElt);
 
 //ajouter un nouvel element
@@ -26,16 +26,41 @@ rubyElt.id = "Ruby";                                      //definition de l'iden
 rubyElt.textContent = "ruby";                             //definition de son contenu textuel
 document.getElementById('langages').appendChild(rubyElt); //insertion du nouvel element
 
-//comme au dessus en changeant createTextNode
+/*//comme au dessus en changeant createTextNode
 let rubyElt = document.createElement("li");               // Création d'un élément li
 rubyElt.id = "ruby";                                      // Définition de son identifiant
 rubyElt.appendChild(document.createTextNode("Ruby"));     // Définition de son contenu textuel
-document.getElementById("langages").appendChild(rubyElt); // Insertion du nouvel élément
+document.getElementById("langages").appendChild(rubyElt); // Insertion du nouvel élément*/
 
 
 var perlElt = document.createElement("li");               // Création d'un élément li
 perlElt.id = "perl";                                      // Définition de son identifiant
 perlElt.textContent = "Perl";                             // Définition de son contenu textuel
-                                // Ajout du nouvel élément avant l'identifiant identifié par "php"
+// Ajout du nouvel élément avant l'identifiant identifié par "php"
 document.getElementById("langages").insertBefore(perlElt, document.getElementById("php"));
+
+document.getElementById('langages').insertAdjacentHTML('afterBegin', '<li id="javascript">JavaScript</li>');
+
+var bashElt = document.createElement("li");               // Création d'un élément li
+bashElt.id = "bash";                                      // Définition de son identifiant
+bashElt.textContent = "Bash";                             // Définition de son contenu textuel
+// Remplacement de l'élément identifié par "perl" par le nouvel élément
+document.getElementById("langages").replaceChild(bashElt, document.getElementById("perl"));
+
+// Suppression de l'élément identifié par "bash"
+document.getElementById("langages").removeChild(document.getElementById("bash"));
+
+
+//Exo1 - ajout de paragraphe
+let pElt = document.createElement('p');
+let lienElt = document.createElement('a');
+lienElt.href = "https://fr.wikipedia.org/wiki/Liste_de_langages_de_programmation";
+lienElt.textContent = 'liste';
+pElt.appendChild(document.createTextNode('En voici une '));
+pElt.appendChild(lienElt);
+pElt.appendChild(document.createTextNode(' plus complete'));
+document.getElementById('contenu').appendChild(pElt);
+
+
+
 
---
+++ OCR_JavaScript-Web/chapitre_05/JS/cours.js
@@ -0,0 +1,86 @@
+/*function clic() {
+    console.log("Clic !");
+}*/
+
+let boutonElt = document.getElementById("bouton");
+boutonElt.addEventListener("click", function(){
+    console.log("Clic !")
+});
+
+// Ajout d'un gestionnaire qui affiche le type et la cible de l'événement
+document.getElementById("bouton").addEventListener("click", function (event) {
+    console.log("Evènement : " + event.type + 
+        ", texte de la cible : " + event.target.textContent);
+});
+
+// Gestion de l'appui sur une touche du clavier produisant un caractère
+/*document.addEventListener("keypress", function (event) {
+    console.log("Vous avez appuyé sur la touche " + String.fromCharCode(event.charCode));
+});*/
+
+
+document.addEventListener('keydown', function(event){
+    console.log('evenement clavier : ' + event.type + ', touche : ' + event.keyCode);
+});
+/*function infoClavier(event){
+    console.log('evenement clavier : ' + event.type + ', touche : ' + event.keyCode);
+}
+document.addEventListener('keyup', infoClavier);*/
+
+
+
+// Renvoie le nom du bouton souris à partir de son code
+function getBoutonSouris(code) {
+    var bouton = "inconnu";
+    switch (code) {
+    case 0: // 0 est le code du bouton gauche
+        bouton = "gauche";
+        break;
+    case 1: // 1 est le code du bouton du milieu
+        bouton = "milieu";
+        break;
+    case 2: // 2 est le code du bouton droit
+        bouton = "droit";
+        break;
+    }
+    return bouton;
+}
+
+// Affiche des informations sur un événement souris
+function infosSouris(e) {
+    console.log("Evènement souris : " + e.type + ", bouton " +
+        getBoutonSouris(e.button) + ", X : " + e.clientX + ", Y : " + e.clientY);
+}
+
+// Gestion du clic souris
+document.addEventListener("click", infosSouris);
+
+// Gestion de l'appui et du relâchement d'un bouton de la souris
+document.addEventListener("mousedown", infosSouris);
+document.addEventListener("mouseup", infosSouris);
+
+// Gestion de la fin du chargement de la page web
+window.addEventListener("load", function () {
+    console.log("Page entièrement chargée");
+});
+
+// Gestion de la fermeture de la page web
+window.addEventListener("beforeunload", function (e) {
+    var message = "On est bien ici !";
+    e.returnValue = message; // Provoque une demande de confirmation (standard)
+    return message; // Provoque une demande de confirmation (certains navigateurs)
+});
+
+
+// Gestion du clic sur le document
+document.addEventListener("click", function () {
+    console.log("Gestionnaire document");
+});
+// Gestion du clic sur le paragraphe
+document.getElementById("para").addEventListener("click", function () {
+    console.log("Gestionnaire paragraphe");
+});
+// Gestion du clic sur le bouton
+document.getElementById("propa").addEventListener("click", function (e) {
+    console.log("Gestionnaire bouton");
+});
